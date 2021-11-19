81c8b9f62fb71c35c73a10f8b701922312ca53ff
jvill171 <juanvillarreal64@yahoo.com>
2021-11-19 14:31:37 -0600

    Added Config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import express from 'express'
+import config from './utils/config'
 
 const app = express()
-
-const port = 3000
 
 app.get('/', (req, res) => {
   res.send({ msg: 'Hello There!' })
 })
 
-app.listen(port)
+app.listen(config.port)
