23b937803c1ddda96902aa84d60b05e8378b2d75
jvill171 <juanvillarreal64@yahoo.com>
2021-11-19 15:02:16 -0600

    Added Cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+
 import config from './utils/config'
 import logger from './utils/logger'
 
 const app = express()
 
 app.use(logger.middleware)
+app.use(helmet())
+app.use(
+  cors({
+    origin: config.origin,
+  }),
+)
 
 app.get('/', (req, res) => {
-  logger.log.info('Doing a call')
+  logger.log.success('Calling Root')
   res.send({ msg: 'Hello There!' })
 })
 
--- src/utils/config.js
+++ src/utils/config.js
@@ -9,6 +9,7 @@ const envSchema = joi
   .keys({
     NODE_ENV: joi.string().valid('development', 'production').required(),
     PORT: joi.number().positive().required(),
+    ORIGIN: joi.string().uri().required(),
   })
   .unknown()
 
@@ -20,4 +21,4 @@ if (error) {
   logger.log.error(new Error(`Config validation error: ${error.message}`))
 }
 
-export default { nodeEnv: env.NODE_ENV, port: env.PORT }
+export default { nodeEnv: env.NODE_ENV, port: env.PORT, origin: env.ORIGIN }
